6988082c0e5da8aa3c6b45c4d047d2064315ffbc
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2024-10-04 00:26:43 +0530

    patch

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 )
+
+injectSpeedInsights();
+inject();
